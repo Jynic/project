875dcf0f09de63ddc6db5e3748a8d08b3d3fa2e4
IMRMT <michaeltimonuli@gmail.com>
2023-10-16 05:57:26 +0700

    CreateCerbungPage1

---
+++ app/src/main/java/com/ivano/project/CreateCerbung1.kt
@@ -0,0 +1,11 @@
+package com.ivano.project
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CreateCerbung1 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_cerbung1)
+    }
+}
