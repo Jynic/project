752ccf0131a13a56a68248c0ac7cebc9d6d76d56
IMRMT <michaeltimonuli@gmail.com>
2023-10-16 06:41:05 +0700

    CreateCerbung page 1,2,3 ui done
    kurang genre di page 3 pake, masih belum tahu

--- app/src/main/java/com/ivano/project/CreateCerbung1Activity.kt
+++ app/src/main/java/com/ivano/project/CreateCerbung1Activity.kt
@@ -3,7 +3,7 @@ package com.ivano.project
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class CreateCerbung1 : AppCompatActivity() {
+class CreateCerbung1Activity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_cerbung1)
--- app/src/main/java/com/ivano/project/CreateCerbung2Activity.kt
+++ app/src/main/java/com/ivano/project/CreateCerbung2Activity.kt
@@ -3,7 +3,7 @@ package com.ivano.project
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class CreateCerbung2 : AppCompatActivity() {
+class CreateCerbung2Activity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_cerbung2)
