8f4cc491982deae9913374123420fdc2b80f0c2e
IMRMT <michaeltimonuli@gmail.com>
2023-10-23 12:44:32 +0700

    Update SignUpActivity.kt

--- app/src/main/java/com/ivano/project/SignUpActivity.kt
+++ app/src/main/java/com/ivano/project/SignUpActivity.kt
@@ -3,6 +3,7 @@ package com.ivano.project
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.ivano.project.databinding.ActivitySignUpBinding
 
 class SignUpActivity : AppCompatActivity() {
@@ -14,8 +15,28 @@ class SignUpActivity : AppCompatActivity() {
         setContentView(view)
 
         binding.btnSignUp.setOnClickListener {
-            val intent = Intent(this, HomeActivity::class.java)
-            startActivity(intent)
+            val username = binding.txtUserSignUp.text.toString()
+            val password = binding.txtPassSignUp.text.toString()
+
+            if (validateSignUp(username, password)) {
+                Toast.makeText(this, "Invalid username or password", Toast.LENGTH_SHORT).show()
+            } else {
+                val intent = Intent(this, HomeActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
         }
+    }
+
+    private fun validateSignUp(username: String, password: String): Boolean {
+        val accounts = Global.accounts
+
+        for (account in accounts) {
+            if (account.username == username && account.password == password) {
+                return true
+            }
+        }
+
+        return false
     }
 }
